d7ab1ce7443d30e5b6fd070895ea31252d4cd3e4
egiros <148718730+egiros@users.noreply.github.com>
2024-01-31 16:58:14 +0100

    up

---
+++ U4-W1-D3/ContoCorrente.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_W1_D3
+{
+    internal class ContoCorrente
+    {
+
+        private bool contoAperto;   
+        private int saldo;
+
+        public ContoCorrente()  // costruttore della classe
+        {
+            contoAperto = false;
+            saldo = 0;
+        }
+
+
+        // 1) Metodo Apertura del conto con un versamento iniziale di almeno 1000
+        public bool ApriConto(int importoIniziale) 
+        {
+            if (!contoAperto && importoIniziale >= 1000) 
+            {
+                contoAperto = true;
+                saldo += importoIniziale;
+                return true; // Restituisce true se il conto viene aperto con successo
+            }
+            else
+            {
+                return false; // Restituisce false se il conto è già aperto o l'importo è inferiore ai 1000
+            }
+        }
+
+        // 2) Metodo Versamento sul conto
+        public bool Versamento(int importo)
+        {
+            if (contoAperto) //il versamento può essere effettuato solo se il conto è aperto, quindi true
+            {
+                saldo += importo;
+                return true; // Restituisce true se il versamento viene effettuato con successo
+            }
+            else
+            {
+                return false; // Restituisce false se il conto non è aperto
+            }
+        }
+
+        // 3) Metodo Prelievo dal conto
+        public bool Prelievo(int importo)
+        {
+            if (contoAperto && saldo >= importo)
+            {
+                saldo -= importo;
+                return true; // Restituisce true se il prelievo viene effettuato con successo
+            }
+            else
+            {
+                return false; // Restituisce false se il conto non è aperto o il saldo è insufficiente
+            }
+        }
+
+        // Uso il metodo per ottenere il saldo del conto
+        public int GetSaldo()
+        {
+            return saldo;
+        }
+    }
+
+}
---
+++ U4-W1-D3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_W1_D3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                ContoCorrente conto = new ContoCorrente();
+
+                Console.WriteLine("Benvenuto in EPIC-BANCA! \nSeleziona un'opzione: \n\n");
+                Console.WriteLine("1. Apri conto");
+                Console.WriteLine("2. Versamento");
+                Console.WriteLine("3. Prelievo");
+                Console.WriteLine("4. Esci");
+
+
+                bool esci = false;  // questa variabile serve per uscire dal ciclo
+
+                while (!esci)
+                {
+                   
+
+                    Console.Write("\n\nInserisci l'opzione desiderata: \n");
+                    int scelta = Convert.ToInt32(Console.ReadLine());
+
+                    switch (scelta)
+                    {
+                        case 1:
+                            Console.Write("\nInserisci l'importo iniziale: ");
+                            int importoIniziale = Convert.ToInt32(Console.ReadLine());
+
+                            if (conto.ApriConto(importoIniziale))
+                            {
+                                Console.WriteLine("\nConto aperto con successo.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nImpossibile aprire il conto. \nControlla l'importo iniziale o verifica se il conto è già aperto.");
+                            }
+                            break;
+
+                        case 2:
+                            Console.Write("\nInserisci l'importo da versare: ");
+                            int importoVersamento = Convert.ToInt32(Console.ReadLine());
+
+                            if (conto.Versamento(importoVersamento))
+                            {
+                                Console.WriteLine($"\nVersamento di {importoVersamento} euro effettuato. \nNuovo saldo: {conto.GetSaldo()} euro.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nImpossibile effettuare il versamento. \nControlla se il conto è aperto.");
+                            }
+                            break;
+
+                        case 3:
+                            Console.Write("\nInserisci l'importo da prelevare: ");
+                            int importoPrelievo = Convert.ToInt32(Console.ReadLine());
+
+                            if (conto.Prelievo(importoPrelievo))
+                            {
+                                Console.WriteLine($"\nPrelievo di {importoPrelievo} euro effettuato. Nuovo saldo: {conto.GetSaldo()} euro.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nImpossibile effettuare il prelievo. Controlla se il conto è aperto o se il saldo è sufficiente.");
+                            }
+                            break;
+
+                        case 4:
+                            esci = true;
+                            Console.WriteLine("\nArrivederci!");
+                            break;
+
+                        default:
+                            Console.WriteLine("\nOpzione non valida. Riprova.");
+                            break;
+                    }
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
